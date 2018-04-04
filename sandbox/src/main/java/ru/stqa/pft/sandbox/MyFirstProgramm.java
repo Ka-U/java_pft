f66cdea2114040e8683733afadee9f0b64fe2100
Ka_U <kau76687>
2018-04-04 15:11:01 +0300

    Добавление класса с функцией для вычиления площади квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -13,12 +13,6 @@ public class MyFirstProgramm {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p1 = new Point(5, 6);
-    Point p2 = new Point(4, 5);
-    String msg = String.format("Расстояние между двумя точками с координатами %s %s и %s %s равно %s",
-              p1.x, p1.y, p2.x, p2.y, p1.distance(p2));
-    System.out.println(msg);
-
   }
 
   public static void hello(String somebody) {
