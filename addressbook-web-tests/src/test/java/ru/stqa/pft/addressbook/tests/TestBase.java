c3ba67b1a85bc861f7256529039f7d5b25e8ab75
Ka_U <kau76687>
2018-04-16 12:29:06 +0300

    Создание двухуровневой архитектуы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+public class TestBase extends ApplicationManager {
+
+    protected final ApplicationManager app = new ApplicationManager();
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        app.init();
+        app.wd.findElement(By.id("container")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        app.stop();
+    }
+
+}
